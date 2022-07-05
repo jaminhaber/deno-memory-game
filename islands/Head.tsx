6fc7aa50bc7e5855acfa9991838fba61f9b9ea7a
Benjamin <benhasanemail@gmail.com>
2022-07-05 21:43:18 +0100

    undo weird broken change

--- islands/Head.tsx
+++ islands/Head.tsx
@@ -1,12 +1,12 @@
 /** @jsx h */
 import { h } from "preact";
-import { Head } from "$fresh/runtime.ts";
+// import { Head } from "$fresh/runtime.ts";
 
 export default function Seo() {
   return (
-    <Head>
+    <head>
       <link rel="icon" href="/logo.png"></link>
       <title>Emoji Memory</title>
-    </Head>
+    </head>
   );
 }
